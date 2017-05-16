cbca151a7807f51bbf1ce433edcd554a16dade6d
Vladislav Guleaev <guleaev.vlad@gmail.com>
2017-05-16 20:43:13 +0300

    Initial commit

---
+++ .NET Projects 2017/Moviemarathon/src/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("MovieMarathon");
+            var info = database.GetCollection<Ticket>("Tickets");
+            
+        }
+    }
+
+    public class Ticket
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
