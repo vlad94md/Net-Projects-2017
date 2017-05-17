15ac82f5485488fd700518f81fdc4152db2b1348
Vladislav Guleaev <guleaev.vlad@gmail.com>
2017-05-18 01:04:14 +0300

    Added tests

---
+++ .NET Projects 2017/Moviemarathon/MongoTests/BsonDocuemtnTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using NUnit.Framework;
+
+namespace MongoTests
+{
+    class BsonDocuemtnTests
+    {
+        public BsonDocuemtnTests()
+        {
+            JsonWriterSettings.Defaults.Indent = true;
+        }
+
+        [Test]
+        public void EmptyDocument()
+        {
+            var document = new BsonDocument();
+
+            Console.WriteLine(document.ToJson());
+        }
+
+        [Test]
+        public void AddElements()
+        {
+            var document = new BsonDocument();
+            document.Add("name", "Vlad");
+
+            Console.WriteLine(document.ToJson());
+        }
+
+        [Test]
+        public void AddElementsToExisting()
+        {
+            var document = new BsonDocument()
+            {
+                {"age", new BsonInt32(23)},
+                {"isAlive", true }
+            };
+
+            document.Add("name", "Vlad");
+
+            Console.WriteLine(document.ToJson());
+        }
+
+        [Test]
+        public void AddArray()
+        {
+            var document = new BsonDocument();
+
+            document.Add("adresses", new BsonArray(new [] {"Str Main 32", "Hyda Park bld"}));
+
+            Console.WriteLine(document.ToJson());
+        }
+
+        [Test]
+        public void EmbedddedDocuments()
+        {
+            var document = new BsonDocument();
+
+            document.Add("contact", new BsonDocument()
+            {
+                {"email", "test@mail.com" },
+                { "telephone", "31-24-56"}
+            });
+
+            Console.WriteLine(document.ToJson());
+        }
+
+        [Test]
+        public void BsonValueConverstion()
+        {
+            var document = new BsonDocument()
+            {
+                {"age", 54}
+            };
+
+            Console.WriteLine(document["age"].AsInt32 + 10);
+            Console.WriteLine(document["age"].ToDouble() + 0.1);
+        }
+    }
+}
---
+++ .NET Projects 2017/Moviemarathon/MongoTests/PocoTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using NUnit.Framework;
+
+namespace MongoTests
+{
+    class PocoTests
+    {
+        public PocoTests()
+        {
+            //JsonWriterSettings.Defaults.Indent = true;
+        }
+
+        [Test]
+        public void Automatic()
+        {
+            var document = new Person()
+            {
+                Name = "Vlad",
+                Age = 23
+            };
+
+            Console.WriteLine(document.ToJson());
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public List<string> Addreses  = new List<string>();
+        }
+    }
+}
