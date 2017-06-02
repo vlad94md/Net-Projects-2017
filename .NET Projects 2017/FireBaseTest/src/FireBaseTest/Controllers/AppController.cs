bca16c19fc561086f757d70bc8147a51fb19806d
vlad94md <i22vladg@mail.ru>
2017-06-02 17:15:50 +0300

    Changes

--- .NET Projects 2017/FireBaseTest/src/FireBaseTest/Controllers/AppController.cs
+++ .NET Projects 2017/FireBaseTest/src/FireBaseTest/Controllers/AppController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Firebase.Database;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -13,7 +14,20 @@ namespace FireBaseTest.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+
+            Method().Wait();
+
             return View();
+        }
+
+        public async Task Method()
+        {
+            var firebase = new FirebaseClient("https://birthdaytest-16f92.firebaseio.com/");
+
+            // add new item to list of data and let the client generate new key for you (done offline)
+            var user = await firebase
+              .Child("users")
+              .PostAsync(new { Name = "Vladislav", Money = "199" });
         }
     }
 }
